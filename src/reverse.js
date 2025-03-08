bb1b524546fdac071996ea8a6c71edc693105613
RODAYNA489 <rodyinfo09@gmail.com>
2025-03-08 10:37:39 -0700

    my first commit

---
+++ src/reverse.js
@@ -0,0 +1,11 @@
+function reverseList(list){
+    let tempVal;
+
+    // use for loop to reverse the list.
+
+for(let i =list.lenght-1;i<=0 ;i--){
+tempVal.push(list[i])
+}
+ return tempVal
+}
+console.log(reverseList([5,4,3,2,1]))
